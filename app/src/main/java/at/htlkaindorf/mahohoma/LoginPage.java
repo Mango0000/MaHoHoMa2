52bc27d76b19c2db305089d0d0fe11b26e61ecf9
Hoemad <hoemad16@htlkaindorf.at>
2020-03-31 19:42:07 +0200

    Login-Page GUI FR

---
+++ app/src/main/java/at/htlkaindorf/mahohoma/LoginPage.java
@@ -0,0 +1,15 @@
+package at.htlkaindorf.mahohoma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
