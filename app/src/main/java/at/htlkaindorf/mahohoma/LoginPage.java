cf40335b25d11ef431c1b3b9be05155a1b81a55f
Hoemad <hoemad16@htlkaindorf.at>
2020-03-31 20:27:24 +0200

    Button Enablen

--- app/src/main/java/at/htlkaindorf/mahohoma/LoginPage.java
+++ app/src/main/java/at/htlkaindorf/mahohoma/LoginPage.java
@@ -3,6 +3,11 @@ package at.htlkaindorf.mahohoma;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -11,5 +16,61 @@ public class LoginPage extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        Button enterButton = findViewById(R.id.enterButton);
+        TextView username = findViewById(R.id.tvUsername);
+        TextView password = findViewById(R.id.tvPassword);
+
+        username.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(username.getText().length()!=0 && password.getText().length()!=0)
+                {
+                    enterButton.setEnabled(true);
+                }
+                else
+                {
+                    enterButton.setEnabled(false);
+                }
+            }
+        });
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(username.getText().length()!=0 && password.getText().length()!=0)
+                {
+                    enterButton.setEnabled(true);
+                }
+                else
+                {
+                    enterButton.setEnabled(false);
+                }
+            }
+        });
+    }
+
+    private void checkTextViews()
+    {
+
     }
 }
